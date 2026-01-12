6c0117a1ab098955e5bc79e5a5bbe34f7af9d4ab
username <>
2026-01-12 18:52:07 -0500

    working on functions

--- rewriting_script.js
+++ rewriting_script.js
@@ -10,38 +10,101 @@
 //the info you need to get, and in what order:
 //the most recent run (how can i figure out if it's complete or not?)
 //the sector that's selected (default 19 which is conus)
-//the frame that's selected (default 1)
+//the frame needed for the hour that's selected; default to 1st frame of the recent run, but if an old run is selected convert the current hour to that run's frame accordingly
 //and the types of info you want to see, like reflectivity, cape, stp, etc.
+
+//default values to start with:
+sector = 19; //conus
+
+//WE ARE USING ZULU TIME
+
 
 async function getMostRecentRun(){
 //first, we can get the fifth oldest run from a url, this is the first thing i found
+///hrrr/status/status.txt might also be useful
 //calculate the most recent run with that +5 hrs
 
 //get the fifth oldest run from matrixinfo.txt
+//format is yyyymmddhh
 	matrixInfo = await fetch('https://www.spc.noaa.gov/exper/hrrr/data/hrrr3/matrixinfo.txt'); //returns a response
 	fifthOldestRun = await matrixInfo.text();
-	console.log("fifthOldestRun (from matrixinfo)", fifthOldestRun);
+	fifthOldestRun = Number(fifthOldestRun);
+	console.log("fifthOldestRun (from matrixinfo.txt)", fifthOldestRun);
 
-	//get the last two digits to just get the hour
-	//then check if it's above 24, if so change the day
-	//we're assuming it's yyyymmddhr
-	fifthOldestHour = fifthOldestRun.slice(8, 10);
-	fifthOldestHour = Number(fifthOldestHour);
-	console.log("fifthOldestHour", fifthOldestHour);
+	//add 5 hrs to that, then get a Date object from that
+	//the Date() constructor accounts for the hours overflowing 23
+	temp_mostRecentRun = fifthOldestRun + 5;
+	temp_mostRecentRun = temp_mostRecentRun.toString();
 
-	mostRecentHour = fifthOldestHour + 5;
-	console.log("mostRecentHour", mostRecentHour);
+	year = temp_mostRecentRun.slice(0, 4);
+	month = temp_mostRecentRun.slice(4, 6);
+	day = temp_mostRecentRun.slice(6, 8);
+	hour = temp_mostRecentRun.slice(8, 10); 
 
-	if (mostRecentHour >= 24) {
-		//get just the fifthOldestRun's date,
-		//then 
+	mostRecentRunDateObject = new Date(year, month, day, hour);
+
+	//build the most recent run's value from that
+	newYear = mostRecentRunDateObject.getFullYear();
+	newMonth = mostRecentRunDateObject.getMonth();
+	newDay = mostRecentRunDateObject.getDate();
+	newHour = mostRecentRunDateObject.getHours();
+
+	newYear = newYear.toString();
+	newMonth = newMonth.toString();
+	newDay = newDay.toString();
+	newHour = newHour.toString();
+
+	function convertNum(num) {
+		if (num < 10) { str = '0' + num.toString() }
+		else { str = num.toString() }
+
+		return str
 	}
+
+	newYear = newYear.toString();
+	newMonth = convertNum(newMonth);
+	newDay = convertNum(newDay);
+	newHour = convertNum(newHour);
+
+	mostRecentRun = newYear + newMonth + newDay + newHour;
+	console.log("mostRecentRun", mostRecentRun);
+
+	return mostRecentRun;
 
 
 }
 
-getMostRecentRun();
+//change the highlighted sector in the sector select img, on mouseover
+function showSector(num) {
+	if (num == 19) { //19 is conus, uses different url
+		document.getElementById('sectormap').src = 'https://www.spc.noaa.gov/exper/hrrr/sectors/sectors_small.gif';
+	}
 
+	else {
+	document.getElementById('sectormap').src = 'https://www.spc.noaa.gov/exper/hrrr/sectors/sector' + num + '.gif';
+	}
+}
+
+//when sector selection img isn't moused over, show the one for the 
+//currently selector sector's global variable
+function showCurrentSector() {
+	showSector(sector);
+}
+
+function selectSector(num) { 
+	sector = num; 
+	updateImg();
+}
+
+function selectFrame(num) {
+
+}
+
+function updateImg() {
+	console.log("todo: update the img");
+}
+
+mostRecentRun = getMostRecentRun();
 
 
 
